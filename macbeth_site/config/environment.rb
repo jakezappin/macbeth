cc1fde85da9097d94e0b5ded4f38cbc79f29a113
jakezappin <jake.zappin@gmail.com>
2017-01-30 01:06:17 -0500

    Fixed root router in rails app

--- macbeth_site/config/environment.rb
+++ macbeth_site/config/environment.rb
@@ -3,10 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# Load the rails application
-require File.expand_path('../application', __FILE__)
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
-# Initialize the rails application
-Stma::Application.initialize!
--- macbeth_site/config/routes.rb
+++ macbeth_site/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'index', to: 'static#index'
+  root to: 'static#index'
 
   get 'display', to: 'static#display'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
